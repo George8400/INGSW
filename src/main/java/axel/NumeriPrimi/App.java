e70887588c56168ee1ee5791f939f34f7dc16919
giorgio.velieri <giorgio.velieri@gmail.com>
2020-10-28 11:23:22 +0100

    First Commit

---
+++ src/main/java/axel/NumeriPrimi/App.java
@@ -0,0 +1,15 @@
+package axel.NumeriPrimi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	MyMath math = new MyMath();
+        System.out.println( math.primeNumbersGenerator(20) );
+    }
+}
---
+++ src/main/java/axel/NumeriPrimi/MyMath.java
@@ -0,0 +1,38 @@
+package axel.NumeriPrimi;
+
+import java.util.List;
+import java.util.LinkedList;
+
+public class MyMath {
+
+	
+	public List<Integer> primeNumbersGenerator(int n) {
+		
+		List<Integer> primeNumbers = new LinkedList<Integer>();
+		
+		if(n >= 2) {
+			primeNumbers.add(2);
+		}
+		for(int i = 3; i <= n; i +=2) {
+			if(this.isPrime(i)) {
+				primeNumbers.add(i);
+			}
+		}
+		
+		return primeNumbers;
+	}
+	
+	public boolean isPrime(int n) {
+		
+		if(n < 2) {
+			return false;
+		}
+		
+		for(int i = 2; i*i < n; i++) {
+			
+		}
+		
+	}
+	
+	
+}
